eb58a1b212bad646fd5a48288b46288c4e44394d
minoltan <minoltan_se15021@kln.ac.lk>
2018-11-25 13:51:58 +0530

    Push Data to DB Checked Succesfully_Only for 2 datas

--- app/src/main/java/com/example/minoltan/citizenapp/SignupActivity.java
+++ app/src/main/java/com/example/minoltan/citizenapp/SignupActivity.java
@@ -1,13 +1,19 @@
 package com.example.minoltan.citizenapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SignupActivity extends AppCompatActivity {
     private EditText mUsername, mEmail, mPhone, mPassword;
@@ -27,7 +33,7 @@ public class SignupActivity extends AppCompatActivity {
 
 
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("User_01");
 
 
 
@@ -35,7 +41,23 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = mUsername.getText().toString().trim();
-                mDatabase.child("Name").setValue(name);
+                String email = mEmail.getText().toString().trim();
+
+                HashMap<String,String> datamap = new HashMap<String, String>();
+                datamap.put("Name", name);
+                datamap.put("Email", email);
+
+
+                mDatabase.push().setValue(datamap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignupActivity.this, "Registered Succesfully", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(SignupActivity.this, "Registere Error", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
 
             }
         });
